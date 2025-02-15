bbc2a850659f1fd1846f5f63541f48cf895f9fa5
denischmkh <avraam.l2004@gmail.com>
2025-02-15 14:58:09 +0200

    Rebase

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ from fastapi import (
 )
 from fastapi.responses import HTMLResponse
 
+active_connections: dict[str, dict[str, WebSocket]] = {}
+
 app = FastAPI()
 
 html = """
@@ -24,6 +26,7 @@ html = """
         <h1>WebSocket Chat</h1>
         <form action="" onsubmit="sendMessage(event)">
             <label>chat: <input type="text" id="chat" autocomplete="off" value="some-key-token"/></label>
+            <label>username: <input type="text" id="username" autocomplete="off" value="username"/></label>
             <button onclick="connect(event)">Connect</button>
             <hr>
             <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
@@ -35,7 +38,8 @@ html = """
             var ws = null;
             function connect(event) {
                 var chat = document.getElementById("chat").value;
-                ws = new WebSocket("wss://todoserviceapi-test.up.railway.app/items/" + chat + "/ws");
+                var username = document.getElementById("username").value;
+                ws = new WebSocket("wss://todoserviceapi-test.up.railway.app/items/" + chat + "/" + username + "/ws");
                 ws.onmessage = function(event) {
                     var messages = document.getElementById('messages');
                     var message = document.createElement('li');
@@ -67,16 +71,30 @@ async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/items/{chat}/ws")
+@app.websocket("/items/{chat}/{username}/ws")
 async def websocket_endpoint(
     *,
     websocket: WebSocket,
-    chat: Annotated[str | None, Path()],
+    chat: Annotated[str, Path()],
+    username: Annotated[str, Path()],
 ):
     await websocket.accept()
+
+    if chat not in active_connections:
+        active_connections[chat] = {}
+
+    active_connections[chat][username] = websocket
+
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(data)
+        for user, socket in active_connections[chat].items():
+            if user == username:
+                continue
+            else:
+                await socket.send_text(data)
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
